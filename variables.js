4735c593a5990968922b9340eb0bfbe13dc00073
Laura Guerra <l.garciaguerra1@gmail.com>
2023-02-03 11:22:31 -0500

    Reassign string values and add console checks

--- variables.js
+++ variables.js
@@ -10,3 +10,18 @@ var statusActive = true;
 var depositPaid = true; 
 var isHungry = false; 
 
+user = "Jackson";
+greeting = "Let's play ball!";
+school = "K9 Camp";
+
+console.log(user);
+console.log(greeting);
+console.log(school);
+
+console.log(age);
+console.log(followerCount);
+console.log(likeCount);
+
+console.log(statusActive);
+console.log(depositPaid);
+console.log(isHungry);
